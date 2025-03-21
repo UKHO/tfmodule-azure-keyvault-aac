23f68f129be8145015239e88498b636fed12ec91
samtitmuss <130447321+samtitmuss@users.noreply.github.com>
2025-03-21 12:40:44 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,11 +5,3 @@ output "keyvault_id" {
 output "appconfig_id" {
     value = azurerm_app_configuration.appconfig.id
 }
-
-output "appconfig_keys_added" {
-    value = keys(azurerm_app_configuration_key.appconfig_keys)
-}
-
-output "appconfig_kv_secrets_added" {
-    value = keys(azurerm_app_configuration_key.kv_secrets)
-}
